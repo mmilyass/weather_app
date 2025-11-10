464310d18908e08e5bbeb41cb8b416c914abb11a
Ilyass Meftah El Menani <imeftah-@c4r7p10.1337.ma>
2025-11-10 11:52:50 +0100

    finish project

--- functions.js
+++ functions.js
@@ -1,21 +1,12 @@
 export const key = "aec57b3783589990abee79b2c67e766b";
 export const key2 = "42f65174c74c433b935152556250710";
 
-const status = document.querySelectorAll("#status");
-const forcast_days = document.querySelectorAll("#temp-one");
-const imgs = document.querySelectorAll("#img-one");
-const imgsTwo = document.querySelectorAll("#img-two");
-const forcast_temps = document.querySelectorAll("#temp-value");
-const tempTwo = document.querySelectorAll("#temp-two");
-const temp_value_one = document.querySelectorAll("#temp-value-one");
-
 export const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 export const months = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ];
 
-const today = new Date();
 
 // Fetch weather data using OpenWeatherMap free API
 export async function fetchedApi(city) {
@@ -101,54 +92,5 @@ export function getWeatherImage(conditionText, hour = 12) {
     return "assets/images/icon-sunny.webp";
 }
 
-// Set forecast days section
-export async function forcastDays(data) {
-    let d = today.getDay();
 
-    for (let i = 0; i < 7; i++) {
-        forcast_days[i].textContent = days[d];
-        d = d === 6 ? 0 : d + 1;
 
-        const forecast = data.forecastData.list[i].main.temp;
-        forcast_temps[i].textContent = Math.round(forecast) + "°";
-        imgs[i].src = getWeatherImage(data.forecastData.list[i].weather[0].description);
-    }
-}
-
-// Set weather status section
-export async function setStatus(data) {
-    status[0].textContent = data.currentData.weather[0].description;
-    status[1].textContent = data.currentData.main.humidity + "%";
-    status[2].textContent = data.currentData.wind.speed + " Km/h";
-    status[3].textContent = '0' + " mm";
-}
-
-// Set hourly forecast section
-export async function hourlyForecast(data) {
-    const localtime = data.hourlyData.location.localtime;
-    let hourData;
-    let check = true;
-    let hour = parseInt(localtime.split(" ")[1].split(":")[0]) + 1;
-    const firstDayHours = data.hourlyData.forecast.forecastday;
-
-    for (let i = 0; i < 8; i++) {
-        if (hour == 24 || check == false) {
-            if (check == true) {
-                check = false;
-                hour = 0;
-            }
-            hourData = firstDayHours[1].hour[hour];
-        }
-        else if (check == true) {
-            hourData = firstDayHours[0].hour[hour];
-        }
-        const ampm = hour >= 12 ? ' PM' : ' AM';
-        const displayHour = hour % 12 || 12;
-
-        imgsTwo[i].classList.remove("hidden");
-        imgsTwo[i].src = getWeatherImage(hourData.condition.text, hour);
-        temp_value_one[i].textContent = Math.round(hourData.temp_c) + "°";
-        tempTwo[i].textContent = displayHour + ampm;
-        hour++;
-    }
-}
--- index.js
+++ index.js
@@ -1,32 +1,116 @@
-import { days, months, getWeatherImage, forcastDays, hourlyForecast, setStatus, fetchedApi } from './functions.js'
+import { days, months, getWeatherImage, fetchedApi } from './functions.js'
 
 const message = document.getElementById("message-error");
 const main = document.querySelector("main");
 const main_img = document.getElementById("main-img");
+const imgsTwo = document.querySelectorAll("#img-two");
+const tempTwo = document.querySelectorAll("#temp-two");
+const temp_value_one = document.querySelectorAll("#temp-value-one");
+const status = document.querySelectorAll("#status");
+const forcast_days = document.querySelectorAll("#temp-one");
+const imgs = document.querySelectorAll("#img-one");
+const forcast_temps = document.querySelectorAll("#temp-value");
+const today = new Date();
+let mesurement = 'C';
+let windMesure = "km/h";
+let data;
+
 let check = true;
 
-async function setMainTemp(data) {
+async function setMainTemp() {
     const today = new Date();
+    let decimalPart;
+    let temp;
     document.getElementById("CityName").textContent = data.currentData.name;
     document.getElementById("date").textContent = days[today.getDay()] + ', ' + months[today.getMonth()] + ', ' + today.getFullYear();
-    let temp = data.currentData.main.temp; 
-    let decimalPart = Math.round(temp) + "°";
+    if (mesurement == 'F') {
+        temp = (data.currentData.main.temp * 9 / 5) + 32;
+        decimalPart = "°" + Math.round(temp);
+    }
+    else {
+        temp = data.currentData.main.temp;
+        decimalPart = Math.round(temp) + "°";
+    }
     document.getElementById("temp").textContent = decimalPart;
     main_img.src = getWeatherImage(data.currentData.weather[0].description);
-    main_img.classList.remove("hidden");
+}
+
+// Set forecast days section
+async function forcastDays(data) {
+    let d = today.getDay();
+    let forecast;
+
+    for (let i = 0; i < 7; i++) {
+        forcast_days[i].textContent = days[d];
+        d = d === 6 ? 0 : d + 1;
+
+        if (mesurement === 'F') {
+            forecast = (data.forecastData.list[i].main.temp * 9 / 5) + 32;
+            forcast_temps[i].textContent = "°" + Math.round(forecast);
+        }
+        else {
+            forecast = data.forecastData.list[i].main.temp;
+            forcast_temps[i].textContent = Math.round(forecast) + "°";
+        }
+        imgs[i].src = getWeatherImage(data.forecastData.list[i].weather[0].description);
+    }
+}
+
+// Set weather status section
+async function setStatus(data) {
+    status[0].textContent = data.currentData.weather[0].description;
+    status[1].textContent = data.currentData.main.humidity + "%";
+    if (windMesure === "km/h")
+        status[2].textContent = data.currentData.wind.speed + " Km/h";
+    else
+        status[2].textContent = ((data.currentData.wind.speed * 0.621370)).toString().slice(0, 4) + " mph";
+    status[3].textContent = '0' + " mm";
+}
+
+// Set hourly forecast section
+async function hourlyForecast(data) {
+    const localtime = data.hourlyData.location.localtime;
+    let hourData;
+    let check = true;
+    let hour = parseInt(localtime.split(" ")[1].split(":")[0]) + 1;
+    const firstDayHours = data.hourlyData.forecast.forecastday;
+    document.getElementById("today").textContent = days[today.getDay()];
+
+    for (let i = 0; i < 8; i++) {
+        if (hour == 24 || check == false) {
+            if (check == true) {
+                check = false;
+                hour = 0;
+            }
+            hourData = firstDayHours[1].hour[hour];
+        }
+        else if (check == true) {
+            hourData = firstDayHours[0].hour[hour];
+        }
+        const ampm = hour >= 12 ? ' PM' : ' AM';
+        const displayHour = hour % 12 || 12;
+
+        imgsTwo[i].classList.remove("hidden");
+        imgsTwo[i].src = getWeatherImage(hourData.condition.text, hour);
+        if (mesurement == 'C')
+            temp_value_one[i].textContent = Math.round(hourData.temp_c) + "°";
+        else
+            temp_value_one[i].textContent = "°" + Math.round(hourData.temp_f);
+        tempTwo[i].textContent = displayHour + ampm;
+        hour++;
+    }
 }
 
 
 document.querySelector("form").addEventListener("submit", async (event) => {
     event.preventDefault();
-    // main.classList.remove("hidden");
     const input = document.getElementById("searchInput").value;
     if (input === "") {
         main.classList.replace("flex", "hidden");
         message.textContent = "Enter a city please!";
     }
     else {
-        const data = await fetchedApi(input);
+        data = await fetchedApi(input);
         if (!data) {
             main.classList.replace("flex", "hidden");
             message.textContent = "City Not found!";
@@ -35,7 +119,7 @@ document.querySelector("form").addEventListener("submit", async (event) => {
         message.classList.add("hidden");
         main.classList.replace("hidden", "flex");
         console.log(data);
-        await setMainTemp(data);
+        await setMainTemp();
         await setStatus(data);
         await forcastDays(data);
         await hourlyForecast(data);
@@ -47,12 +131,10 @@ function units(target) {
     target.stopPropagation();
     if (check == true) {
         check = false;
-        document.querySelector("header").style.opacity = "0.8";
         document.getElementById("navig").classList.replace("hidden", "flex");
     }
     else {
         check = true;
-        document.querySelector("header").style.opacity = "1";
         document.getElementById("navig").classList.replace("flex", "hidden");
     }
 }
@@ -61,20 +143,38 @@ function units(target) {
 // those functions are responsible for the temperature and wind mesure 
 // calculation and representation .
 
-function TempCels(){
+function TempCels() {
+    mesurement = 'C';
+    if (data) {
+        hourlyForecast(data);
+        setMainTemp(data);
+        forcastDays(data);
+    }
     document.getElementById("celsLabel").textContent = "✔";
     document.getElementById("fahLabel").textContent = "";
 }
-function TempFah(){
+function TempFah() {
+    mesurement = 'F';
+    if (data) {
+        hourlyForecast(data);
+        setMainTemp(data);
+        forcastDays(data);
+    }
     document.getElementById("celsLabel").textContent = "";
     document.getElementById("fahLabel").textContent = "✔";
 }
 
-function WindKm(){
+function WindKm() {
+    windMesure = "km/h";
+    if (data)
+        setStatus(data);
     document.getElementById("kmLabel").textContent = "✔";
     document.getElementById("mphLabel").textContent = "";
 }
-function WindMph(){
+function WindMph() {
+    windMesure = "mph";
+    if (data)
+        setStatus(data);
     document.getElementById("kmLabel").textContent = "";
     document.getElementById("mphLabel").textContent = "✔";
 }
@@ -82,14 +182,9 @@ function WindMph(){
 // we are adding here an event listere for the nav bar.
 document.getElementById("dropdown-one").addEventListener("click", units);
 document.getElementById("units").addEventListener("click", units);
-document.getElementById("navig").addEventListener("click", target =>{
-    target.stopPropagation();
-})
-document.addEventListener("click", () =>{
-    if (check == false)
-    {
+document.addEventListener("click", () => {
+    if (check == false) {
         check = true;
-        document.querySelector("header").style.opacity = "1";
         document.getElementById("navig").classList.replace("flex", "hidden");
     }
 })
